f6c3493b718c38a23c212367951ad3708c5fed46
Adespscientist <79201846+Adespscientist@users.noreply.github.com>
2023-03-10 22:46:50 +0100

    new

--- src/components/Video.jsx
+++ src/components/Video.jsx
@@ -1,14 +1,15 @@
 import React from "react";
-import video from '../utils/assets/video.mp4'
-
-const Video =()=>{
-    return(
-            <div className="video">
-                <div className="video__container">
-                <video src={video} autoPlay loop />
-                </div>
-                
-            </div>
-    )   
-}
-export default Video
+import video from "../utils/assets/video.mp4";
+import imag from '../utils/assets/639cdb35f8822842f930e577_billable_home.webp'
+const Video = () => {
+  return (
+    <div className="video">
+      <div className="video__container">
+        <video controls autoPlay loop>
+          <source src={video} type="video/mp4" />
+        </video>
+      </div>
+    </div>
+  );
+};
+export default Video;
