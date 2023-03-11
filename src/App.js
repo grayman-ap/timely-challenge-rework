e7cad8e07bd10c406a04767a870e964ebc89c9bc
Adespscientist <79201846+Adespscientist@users.noreply.github.com>
2023-03-11 01:58:55 +0100

    new

--- src/App.js
+++ src/App.js
@@ -5,14 +5,12 @@ function App() {
   return (
     <Router>
     <div className="app" style={{color:color.white}}>
-
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="pricing" element={<Pricing />} />
           <Route path="login" element={<Login />} />
           <Route path="sales" element={<Sales />} />
         </Routes>
- 
     </div>
     </Router>
   );
