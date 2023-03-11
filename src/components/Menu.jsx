e7b366888dcc62f46c97e861143155efce19a059
Adespscientist <79201846+Adespscientist@users.noreply.github.com>
2023-03-11 17:00:47 +0100

    new

---
+++ src/components/Menu.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function Menu() {
+  return (
+    <div className="menu">
+        <div className="menu__container">
+            
+        </div>
+        <span></span>
+        <span></span>
+        <span></span>
+    </div>
+  )
+}
+
+export default Menu
